8b81f0ae8dcabd099e6671c20906876c8567c09a
6ity <121387494+6ity@users.noreply.github.com>
2022-12-27 20:58:32 +0300

    Update PareX V1.lua

--- PareX V1.lua
+++ PareX V1.lua
@@ -1,6 +1,6 @@
 game.StarterGui:SetCore("SendNotification", {
                     Title = "✅ Executed";
-                    Text = "Resolver has been successfully executed!",
+                    Text = "Lock has been successfully executed!",
                     Icon = "http://www.roblox.com/asset/?id=8709610734",
                     Duration = 3
    })
