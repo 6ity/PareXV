3acd148f9cd3d34df2da3051fdc7281df139ed43
6ity <121387494+6ity@users.noreply.github.com>
2022-12-27 19:28:06 +0300

    Update PareX Anti.lua

--- PareX Anti.lua
+++ PareX Anti.lua
@@ -1,1 +1,11 @@
-# PareXV
+getgenv().Sky = true
+getgenv().SkyAmount = 100
+
+game:GetService("RunService").heartbeat:Connect(function()
+    if getgenv().Sky ~= false then 
+    local vel = game.Players.LocalPlayer.Character.HumanoidRootPart.Velocity
+    game.Players.LocalPlayer.Character.HumanoidRootPart.Velocity = Vector3.new(0,      getgenv().SkyAmount,0) 
+    game:GetService("RunService").RenderStepped:Wait()
+    game.Players.LocalPlayer.Character.HumanoidRootPart.Velocity = vel
+    end 
+end)
