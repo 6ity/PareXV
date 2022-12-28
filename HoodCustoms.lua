9939a79e44619c4f1477cf4c09489b78737db99a
6ity <121387494+6ity@users.noreply.github.com>
2022-12-28 19:58:27 +0300

    Update HoodCustoms.lua

--- HoodCustoms.lua
+++ HoodCustoms.lua
@@ -1,6 +1,6 @@
 game.StarterGui:SetCore("SendNotification", {
                     Title = "✅ Executed";
-                    Text = "Hood Customs Lock has been successfully executed!",
+                    Text = "Hood Customs Lock has been successfully executed! Press 'e' to enable",
                     Icon = "http://www.roblox.com/asset/?id=8709610734",
                     Duration = 3
 
